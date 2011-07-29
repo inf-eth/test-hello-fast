108ddc3aa454d1dbc99c4421a93470b8850651ca
infinity.ethereal <infinity.ethereal@0468d34c-6dbe-8d29-ce39-d02b7d450285>
2011-07-29 15:55:53 +0000

    Changed the file a bit. Note the changes. Does this compile on linux and/or windows?

--- trunk/Sys0.cpp
+++ trunk/Sys0.cpp
@@ -1,7 +1,13 @@
 #include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 int main ( )
 {
-	system ( "./out.o" );
+	// The system command can be used in windows or linux to access shell (DOS for windows or BASH etc for linux). This command tries to run an executible called out.o.
+	system("./out.o");
+
+	// Testing lalalala!
+	cout << "Hello world." << endl;
 	return 0;
 }
