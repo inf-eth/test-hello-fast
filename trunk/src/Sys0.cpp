d3ffd5adb0b8a5a55c30efb0d898667714725b5b
infinity.ethereal <infinity.ethereal@0468d34c-6dbe-8d29-ce39-d02b7d450285>
2011-08-01 00:11:55 +0000

    Removed "hello world" for now.

--- trunk/src/Sys0.cpp
+++ trunk/src/Sys0.cpp
@@ -2,12 +2,11 @@
 #include <iostream>
 using namespace std;
 
-int main ( )
+int main()
 {
-	// The system command can be used in windows or linux to access shell (DOS for windows or BASH etc for linux). This command tries to run an executible called out.o.
+	// The system command can be used in windows or linux to access shell (DOS for windows or BASH etc for linux). 
+	// It tries to run an executible called out.o.
 	system("./out.o");
-
-	// Testing lalalala!
-	cout << "Hello world." << endl;
+	
 	return 0;
 }
