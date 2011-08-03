0fa1d47cc5b80bafe1eabe0ff8dce6a608592869
attique.dawood@nu.edu.pk <attique.dawood@nu.edu.pk@0468d34c-6dbe-8d29-ce39-d02b7d450285>
2011-08-03 01:04:11 +0000

    - Display directory contents using System(). - *.opt, *.ncb and *.plg should be non-versioned.

--- trunk/src/Sys0.cpp
+++ trunk/src/Sys0.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main()
 {
 	// The system command can be used in windows or linux to access shell (DOS for windows or BASH etc for linux). 
-	// It tries to run an executible called out.o.
-	system("./out.o");
-	
+	// It tries to run an executible called out.o assuming linux shell.
+	system("./out.o");			// Won't run on windows.
+	system("dir");				// Display directory contents in windows.
+	system("ls -la");			// Display directory contents in linux.
 	return 0;
 }
